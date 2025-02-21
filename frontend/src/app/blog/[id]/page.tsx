dd91a8367b85e7c904a34e2585da5c843e4eca00
Tai <hotai1806@gmail.com>
2025-02-21 11:14:26 +0700

    [Update] Dynamic page of blog page

--- frontend/src/app/blog/[id]/page.tsx
+++ frontend/src/app/blog/[id]/page.tsx
@@ -1,63 +1,45 @@
-"use client";
 import Link from "next/link";
 
 import { gql } from "@apollo/client";
-// import client from "@/app/lib/apolloClient";
+import { getClient } from "../../../app/lib/apolloClient";
 
-const GET_POSTS = gql`
-  query {
-    posts {
-      title
-      updatedAt
-      tags
-      slug
-      published
-      id
-      createdAt
+const GET_POSTS_BY_ID = gql`
+  query ($postId: ID) {
+    post(id: $postId) {
+      author {
+        username
+      }
+      comments {
+        author {
+          username
+        }
+      }
       content
+      createdAt
+      id
+      title
     }
   }
 `;
 
-interface BlogPostParams {
-  params: {
-    id: string;
-  };
-}
-interface BlogPost {
-  id: string;
-  title: string;
-  content: string;
-  date: string;
-  slug: string;
-  comments: {
-    id: string;
-    content: string;
-    date: string;
-  };
-  tags: string[];
-  createdAt: string;
-}
+type Params = Promise<{ id: string }>;
 
-export async function getStaticProps() {
-  // const { data } = await client.query({ query: GET_POSTS });
-  // Render the first 10 posts at build time
-  // return data.slice(0, 10).map((post: BlogPost) => ({
-  //   slug: post.slug,
-  // }));
-}
-
-export default function BlogPostPage({ params }: BlogPostParams) {
+export default async function BlogPostPage(props: { params: Params }) {
   // In a real app, you would fetch the post data based on the ID
+  const client = getClient();
+  const { data } = await client.query({
+    query: GET_POSTS_BY_ID, // this will query the id only
+    variables: {
+      postId: "1",
+    },
+  });
+  const id = (await props.params).id;
+
   const post = {
-    id: params.id,
-    title: "Getting Started with Next.js App Router",
-    content: `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      `,
+    id: id,
+
+    title: data.post.title,
+    content: data.post.content,
     date: "2025-02-12",
     author: {
       name: "John Doe",
